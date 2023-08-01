d9338bd9e115867391d8cc555e388cc2e6eabb9b
dimavolkov494 <83434072+dimavolkov494@users.noreply.github.com>
2023-08-01 22:15:41 +0300

    Update main.py
    region added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import re
 
 class Lot:
-	def __init__(self, torg_n, lot, money, link, prop, vin):
+	def __init__(self, torg_n, lot, money, link, prop, vin, region):
 		self.lot = lot.text.replace('                                    ','')
 		self.lot = self.lot.replace('\n', '')
 		self.money = money.text
@@ -13,13 +13,15 @@ class Lot:
 		self.properties = prop
 		self.torg_n = torg_n
 		self.vin = vin
+		self.region = region
 
 		self.libra = {"Номер на торгах" : str(self.torg_n),
 						"Лот" : self.lot.split('Лот: ')[1],
 						"Цена" : float(self.money.replace(" ","")),
 						"VIN номер" : self.vin,
 						"Ссылка" : self.link,
-						"Описание" : self.properties
+						"Описание" : self.properties,
+						"Регион" : self.region
 						}
 
 	def show(self):
@@ -48,25 +50,37 @@ def parse(url):
 		newbrowser.get(link)
 		time.sleep(2)
 
+		#properties
 		new_bs = BeautifulSoup(newbrowser.page_source, 'html.parser')
 		prop = new_bs.find_all('span', {"content":"leaf:DebtorBidName","id":"DynamicControlBidInfo_DebtorBidName", "name":"DynamicControlBidInfo_DebtorBidName"})
-		
+		region = new_bs.find_all('span', {"content":"leaf:BidRegion","id":"DynamicControlBidInfo_BidRegion", "name":"DynamicControlBidInfo_BidRegion"})
+
+		#properties
 		if len(prop) == 0:
 			propt = '123'
 		else:	
 			propt = prop[0].text
 
+		#city
+		if len(region) == 0:
+			region_str = '123'
+		else:	
+			region_str = region[0].text
 
+		#vin
 		match = re.search(r'[A-Za-z0-9]{16}', propt)
 
-		# Если ключевая строка найдена, вывод ее на экран
+		# Если ключевая строка найдена
 		if match:
 			key_string = match.group()
 			vin = key_string
 		else:
 			vin = "Не удалось найти VIN"
 
-		obj = Lot(torg_number, lot, money, link, propt, vin)
+
+
+
+		obj = Lot(torg_number, lot, money, link, propt, vin, region_str)
 		obj.show()
 		arrayoflots.append(obj)
 		for lot in arrayoflots:
